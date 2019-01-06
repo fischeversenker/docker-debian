052f90cc8d53323efc0c8bbd59dd8a645f6451d1
fischeversenker <fischeversenker@users.noreply.github.com>
2019-01-06 22:51:31 +0100

    Delete sync_lineage-16.0.sh

--- shared/sync_lineage-16.0.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mkdir -p lineage-16.0
-cd lineage-16.0
-repo init --depth=1 -u https://github.com/LineageOS/android.git -b lineage-16.0
-repo sync -f --force-sync --force-broken --no-clone-bundle --no-tags -j2
